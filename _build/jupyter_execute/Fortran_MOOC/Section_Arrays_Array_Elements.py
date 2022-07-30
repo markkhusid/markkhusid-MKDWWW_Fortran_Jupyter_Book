054f61b585e8e982fd05467416b1ab89562fa6be
Mark Khusid <markkhusid@protonmail.com>
2022-07-30 12:39:47 -0400

    array elements and array arithmetic got crossed

--- _build/jupyter_execute/Fortran_MOOC/Section_Arrays_Array_Elements.py
+++ _build/jupyter_execute/Fortran_MOOC/Section_Arrays_Array_Elements.py
@@ -11,36 +11,24 @@ kernelspec:
   language: python
   name: python3
 ---
-# # Section Arrays: Array Arithmetic
+# # Section Arrays: Array Elements
 
 # Adapted from: [https://github.com/gjbex/Fortran-MOOC/tree/master/source_code/arrays](https://github.com/gjbex/Fortran-MOOC/tree/master/source_code/arrays)
 
-# ## This program demonstrates array arithmetic.
+# ## This program demonstrates array element indexing.
 
 # ```fortran
-# program array_arithmetic
+# program array_elements
 #     implicit none
-#     real, dimension(5) :: A = [ 1.2, 2.3, 3.4, 4.5, 5.6 ], &
-#                           B = [ -1.0, -0.5, 0.0, 0.5, 1.0], &
-#                           C
+#     integer, dimension(5) :: A
+#     integer :: i
 # 
-#     print *, "The array A contains: "
+#     A(1) = 1
+#     do i = 2, size(A)
+#         A(i)  = 2*A(i - 1)
+#     end do
 #     print *, A
-# 
-#     print *
-# 
-#     print *, "The array B contains: "
-#     print *, B
-# 
-#     print *
-# 
-#     C = A + 2.0*B
-# 
-#     print *, "C = A + 2.0 * B"
-#     print *, "The array C contains: "
-# 
-#     print *, C
-# end program array_arithmetic
+# end program array_elements
 # ```
 
 # The above program is compiled and run using Fortran Package Manager (fpm):
@@ -55,7 +43,7 @@ root_dir = os.getcwd()
 # In[2]:
 
 
-code_dir = root_dir + "/" + "Fortran_Code/Section_Arrays_Array_Arithmetic"
+code_dir = root_dir + "/" + "Fortran_Code/Section_Arrays_Array_Elements"
 
 
 # In[3]:
