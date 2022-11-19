77e7e73855a443aa014487e4975ad4d4fe06f2fc
Mark Khusid <markkhusid@protonmail.com>
2022-11-19 16:47:59 -0500

    rebuilt entire book

--- _build/jupyter_execute/Fortran_MOOC/Section_Arrays_Array_Initialization.py
+++ _build/jupyter_execute/Fortran_MOOC/Section_Arrays_Array_Initialization.py
@@ -56,7 +56,7 @@ os.chdir(code_dir)
 # In[4]:
 
 
-build_status = os.system("fpm build > /dev/null")
+build_status = os.system("fpm build 2>/dev/null")
 
 
 # fpm run output is piped into sed to suppress the status of the run command and only print the output of the executable.
