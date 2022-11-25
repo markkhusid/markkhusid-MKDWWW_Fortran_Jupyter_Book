fd7746845c9b26ffd6d790cdb699a07de6441b55
Mark Khusid <markkhusid@protonmail.com>
2022-11-25 14:55:09 -0500

    added arrays section elemental subroutines

---
+++ Fortran_MOOC/Fortran_Code/Section_Arrays_Elemental_Subroutine/src/Section_Arrays_Elemental_Subroutine.f90
@@ -0,0 +1,10 @@
+module Section_Arrays_Elemental_Subroutine
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, Section_Arrays_Elemental_Subroutine!"
+  end subroutine say_hello
+end module Section_Arrays_Elemental_Subroutine
