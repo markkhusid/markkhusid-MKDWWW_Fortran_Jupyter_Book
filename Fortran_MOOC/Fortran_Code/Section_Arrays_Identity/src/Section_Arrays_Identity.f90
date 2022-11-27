7728bcd8ef9f4809803720afc06beda98b96a3d1
Mark Khusid <markkhusid@protonmail.com>
2022-11-27 11:43:29 -0500

    added Fortran MOOC section arrays identity

---
+++ Fortran_MOOC/Fortran_Code/Section_Arrays_Identity/src/Section_Arrays_Identity.f90
@@ -0,0 +1,10 @@
+module Section_Arrays_Identity
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, Section_Arrays_Identity!"
+  end subroutine say_hello
+end module Section_Arrays_Identity
