0a1ad2a38724990bf9ac7181ee94fb72770a9bf1
Mark Khusid <markkhusid@protonmail.com>
2022-12-06 06:29:25 -0500

    added section arrays reshape

---
+++ Fortran_MOOC/Fortran_Code/Section_Arrays_Reshape/src/Section_Arrays_Reshape.f90
@@ -0,0 +1,10 @@
+module Section_Arrays_Reshape
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, Section_Arrays_Reshape!"
+  end subroutine say_hello
+end module Section_Arrays_Reshape
