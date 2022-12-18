2d4156633741d855da7154172e0e0858507928f1
Mark Khusid <markkhusid@protonmail.com>
2022-12-18 00:01:48 -0500

    finished section arrays

--- _build/jupyter_execute/Fortran_MOOC/Section_Arrays_Trace.py
+++ _build/jupyter_execute/Fortran_MOOC/Section_Arrays_Trace.py
@@ -16,6 +16,46 @@ kernelspec:
 
 # Adapted from: [https://github.com/gjbex/Fortran-MOOC/tree/master/source_code/arrays](https://github.com/gjbex/Fortran-MOOC/tree/master/source_code/arrays)
 
+# ## This program demonstrates computing the trace of a matrix  in Fortran.
+
+# ```fortran
+# program compute_trace
+#     implicit none
+#     integer :: i
+#     real, dimension(5, 5) :: A
+# 
+#     call random_number(A)
+#     do i = 1, size(A, 1)
+#         print '(5F7.4)', A(i, :)
+#     end do
+#     print '(/, A, F7.4)', 'trace = ', trace(A)
+# 
+# contains
+# 
+#     real function trace(matrix)
+#         use, intrinsic :: iso_fortran_env, only : error_unit
+#         implicit none
+#         real, dimension(:, :), intent(in) :: matrix
+#         integer :: i
+# 
+#         if (size(matrix, 1) /= size(matrix, 2)) then
+#             write (unit=error_unit, fmt='(A)') &
+#                 'error: can not compute trace of a non-square matrix'
+#             stop 1
+#         end if
+#         trace = 0.0
+#         do i = 1, size(matrix, 1)
+#             trace = trace + matrix(i, i)
+#         end do
+#     end function trace
+# 
+# end program compute_trace
+# ```
+
+# The above program is compiled and run using Fortran Package Manager (fpm):
+
+# ## Build the Program using FPM (Fortran Package Manager)
+
 # In[1]:
 
 
@@ -27,38 +67,31 @@ root_dir = os.getcwd()
 
 
 code_dir = root_dir + "/" + "Fortran_Code/Section_Arrays_Trace"
-code_app_dir = code_dir + "/" + "app"
 
 
 # In[3]:
 
 
-os.chdir(code_app_dir)
+code_app_dir = code_dir + "/" + "app"
 
-
-# ## This program demonstrates computing the trace of a matrix  in Fortran.
 
 # In[4]:
 
 
-get_ipython().run_cell_magic('capture', '', "%%writefile section_arrays_trace.f90\nprogram compute_trace\n    implicit none\n    integer :: i\n    real, dimension(5, 5) :: A\n\n    call random_number(A)\n    do i = 1, size(A, 1)\n        print '(5F7.4)', A(i, :)\n    end do\n    print '(/, A, F7.4)', 'trace = ', trace(A)\n\ncontains\n\n    real function trace(matrix)\n        use, intrinsic :: iso_fortran_env, only : error_unit\n        implicit none\n        real, dimension(:, :), intent(in) :: matrix\n        integer :: i\n\n        if (size(matrix, 1) /= size(matrix, 2)) then\n            write (unit=error_unit, fmt='(A)') &\n                'error: can not compute trace of a non-square matrix'\n            stop 1\n        end if\n        trace = 0.0\n        do i = 1, size(matrix, 1)\n            trace = trace + matrix(i, i)\n        end do\n    end function trace\n\nend program compute_trace")
+os.chdir(code_app_dir)
 
 
 # In[5]:
 
 
-get_ipython().system('bat section_arrays_trace.f90')
+get_ipython().run_cell_magic('capture', '', "%%writefile section_arrays_trace.f90\nprogram compute_trace\n    implicit none\n    integer :: i\n    real, dimension(5, 5) :: A\n\n    call random_number(A)\n    do i = 1, size(A, 1)\n        print '(5F7.4)', A(i, :)\n    end do\n    print '(/, A, F7.4)', 'trace = ', trace(A)\n\ncontains\n\n    real function trace(matrix)\n        use, intrinsic :: iso_fortran_env, only : error_unit\n        implicit none\n        real, dimension(:, :), intent(in) :: matrix\n        integer :: i\n\n        if (size(matrix, 1) /= size(matrix, 2)) then\n            write (unit=error_unit, fmt='(A)') &\n                'error: can not compute trace of a non-square matrix'\n            stop 1\n        end if\n        trace = 0.0\n        do i = 1, size(matrix, 1)\n            trace = trace + matrix(i, i)\n        end do\n    end function trace\n\nend program compute_trace")
 
-
-# The above program is compiled and run using Fortran Package Manager (fpm):
 
 # In[6]:
 
 
 os.chdir(code_dir)
 
-
-# ## Build the Program using FPM (Fortran Package Manager)
 
 # In[7]:
 
