3919847fa3e8b60f9f46d79cf7078e544cfd78d5
Mark Khusid <markkhusid@protonmail.com>
2022-12-17 22:11:28 -0500

    added section arrays trace

---
+++ Fortran_MOOC/Fortran_Code/Section_Arrays_Trace/src/Section_Arrays_Trace.f90
@@ -0,0 +1,10 @@
+module Section_Arrays_Trace
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, Section_Arrays_Trace!"
+  end subroutine say_hello
+end module Section_Arrays_Trace
