560650ce276150cbe557d94cf6a3b5e706cea5aa
Mark Khusid <markkhusid@protonmail.com>
2022-12-23 14:16:19 -0500

    added compute pi section

---
+++ Fortran_MOOC/Fortran_Code/Section_Computing_Pi_Compute_Pi/src/Section_Computing_Pi_Compute_Pi.f90
@@ -0,0 +1,10 @@
+module Section_Computing_Pi_Compute_Pi
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, Section_Computing_Pi_Compute_Pi!"
+  end subroutine say_hello
+end module Section_Computing_Pi_Compute_Pi
