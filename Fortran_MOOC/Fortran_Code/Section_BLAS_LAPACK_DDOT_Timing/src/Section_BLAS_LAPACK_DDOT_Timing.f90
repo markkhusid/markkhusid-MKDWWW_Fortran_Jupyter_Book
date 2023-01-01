8c68b047248e577f478f37b7164812b8eddf68f7
Mark Khusid <markkhusid@protonmail.com>
2023-01-01 13:04:22 -0500

    added section ddot timing

---
+++ Fortran_MOOC/Fortran_Code/Section_BLAS_LAPACK_DDOT_Timing/src/Section_BLAS_LAPACK_DDOT_Timing.f90
@@ -0,0 +1,10 @@
+module Section_BLAS_LAPACK_DDOT_Timing
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, Section_BLAS_LAPACK_DDOT_Timing!"
+  end subroutine say_hello
+end module Section_BLAS_LAPACK_DDOT_Timing
