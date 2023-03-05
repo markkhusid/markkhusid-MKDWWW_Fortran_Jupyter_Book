e0e5caea4ea2dfe7f8afbaeea92c20b3e2798c24
Mark Khusid <markkhusid@protonmail.com>
2023-03-05 13:07:23 -0500

    added blas/lapack scal page

--- Fortran_MOOC/Fortran_Code/Section_BLAS_LAPACK_Scal/app/section_blas_lapack_scal.f90
+++ Fortran_MOOC/Fortran_Code/Section_BLAS_LAPACK_Scal/app/section_blas_lapack_scal.f90
@@ -6,21 +6,41 @@ program scal
   real(kind=DP), dimension(rows) :: norm
   integer :: row
 
+  ! Initialize matrix with random numbers
   call random_number(matrix)
+  
+  ! Print original matrix
   print '(A)', 'original:'
+
+  ! Print matrix
   call print_matrix(matrix)
 
+  ! Normalize along each column
   norm = sum(matrix, dim=2)
   print '(A, *(F12.7))', 'norm:', norm
 
+  ! Loop over rows
   do row = 1, rows
       print '(A, I0)', 'row: ', row
+      print '(A, F12.7)', '1/norm(row): ', 1.0_DP/norm(row)
+
+      ! In Fortran, the function call for dscal is:
+      ! call dscal(n, da, dx, incx)
+      ! where:
+      ! n is the number of elements in the vector
+      ! da is the scalar
+      ! dx is the vector
+      ! incx is the increment between elements of the vector
       call dscal(cols, 1.0_DP/norm(row), matrix(row, :), 1)
+      
+      ! Print the matrix
       call print_matrix(matrix)
   end do
   print '(A)', 'row-normalized::'
+  ! Print matrix
   call print_matrix(matrix)
 
+  ! Normalize along each column
   norm = sum(matrix, dim=2)
   print '(A, *(F12.7))', 'norm:', norm
 
