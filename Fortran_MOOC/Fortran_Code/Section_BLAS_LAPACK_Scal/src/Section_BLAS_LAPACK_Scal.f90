dc830337593a8716fb3c81d6c0de3dea99186667
Mark Khusid <markkhusid@protonmail.com>
2023-03-05 12:22:12 -0500

    updates

---
+++ Fortran_MOOC/Fortran_Code/Section_BLAS_LAPACK_Scal/src/Section_BLAS_LAPACK_Scal.f90
@@ -0,0 +1,10 @@
+module Section_BLAS_LAPACK_Scal
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, Section_BLAS_LAPACK_Scal!"
+  end subroutine say_hello
+end module Section_BLAS_LAPACK_Scal
